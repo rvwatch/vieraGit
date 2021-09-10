8bea50458aa001a5ce5afc7a224a1976d961ef69
rvwatch <astro.viera@gmail.com>
2021-09-10 14:48:54 -0600

    Moves API calls to own file

---
+++ src/getData.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { buildReposQuery, buildPullQuery } from './buildQuery';
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+export const getPullRequestsForRepoPage = async (options) => {
+  try {
+    const response = await axios('https://api.github.com/graphql', {
+      method: 'POST',
+      data: JSON.stringify(buildPullQuery(options)),
+      headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`,
+      },
+    });
+
+    const { pullRequests } = response.data.data.organization.repository;
+    const after = pullRequests.pageInfo.hasNextPage && pullRequests.pageInfo.endCursor;
+    const prs = pullRequests.edges.map(edge => edge.node);
+
+    return { prs, after };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const getReposPage = async (options) => {
+  try {
+    const response = await axios('https://api.github.com/graphql', {
+      method: 'POST',
+      data: JSON.stringify(buildReposQuery(options)),
+      headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`,
+      },
+    });
+
+    const { repositories } = response.data.data.organization;
+    const after = repositories.pageInfo.hasNextPage && repositories.pageInfo.endCursor;
+    const page = repositories.nodes.map(repo => repo.name);
+
+    return { page, after };
+  } catch (err) {
+    console.log(err);
+  }
+}
