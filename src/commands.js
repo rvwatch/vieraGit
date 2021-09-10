fc4695d0ad237bdbfbcd058122be36c0593a7d95
rvwatch <astro.viera@gmail.com>
2021-09-10 15:14:47 -0600

    more cleanup

--- src/commands.js
+++ src/commands.js
@@ -1,25 +1,10 @@
-import { getAllPullRequests } from './query';
+import { gatherAllPullRequests } from './cliFunctions';
 
-
+// Going to try and parse out flags that I pass in to node via process.argv
+let flags = process.argv.slice(2);
+console.log(flags)
 const processParams = (params) => {
-  for(let i = 0; i < params.length; i += 2){
-    console.log('Your Params', params[i], params[i + 1]);
-    let missingDash = !!params[i].indexOf('-');
-    console.log('Missing a dash?', missingDash)
-
-    if((params[i] && params[i + 1]) && missingDash){
-
-      console.log('You forgot a "-" or "--" in front of', params[i])
-    }
-    if(params[i] && !missingDash && !params[i + 1]){
-      console.log('This type of arg must follow', params[i])
-    }
-  }
-}
-
-
-export async function commands (args) {
-  // let params = args.slice(2);
+    // let params = args.slice(2);
 
   // if(!params.length){
   //   console.log('Pass in some flags: ex --')
@@ -27,6 +12,22 @@ export async function commands (args) {
   // } else {
   //   processParams(params);
   // }
+  for (let i = 0; i < params.length; i += 2) {
+    console.log('Your Params', params[i], params[i + 1]);
+    let missingDash = !!params[i].indexOf('-');
+    console.log('Missing a dash?', missingDash)
 
-  await getAllPullRequests();
+    if ((params[i] && params[i + 1]) && missingDash) {
+
+      console.log('You forgot a "-" or "--" in front of', params[i])
+    }
+    if (params[i] && !missingDash && !params[i + 1]) {
+      console.log('This type of arg must follow', params[i])
+    }
+  }
+}
+
+
+export async function commands(args) {
+  await gatherAllPullRequests(args);
 }
